ee734c4c379407b0037cbffea082b052962f90a1
gwenwu <gwnwu@outlook.com>
2025-03-29 16:07:55 +0800

    add error template and base template

--- app.py
+++ app.py
@@ -66,18 +66,24 @@ class Movie(db.Model): # 表名movie
     title = db.Column(db.String(60)) # 电影标题
     year = db.Column(db.String(4)) # 电影年份
 
+# 模版上下文处理函数
+@app.context_processor
+def inject_user(): # 函数名可以随意修改
+    user = User.query.first()
+    return dict(user=user) # 需要返回字段，等同于return {'user':user}
+
+@app.errorhandler(404) # 传入要处理的错误代码
+def page_not_found(e): # 接受异常对象作为参数
+    user = User.query.first()
+    return render_template('404.html', user=user), 404 # 返回模版和状态码
+
 # 在主页视图读取数据库记录
 @app.route('/')
 def index(): # 因为设置了数据库，负责显示主页的index可以从数据库里读取真实的数据
     user = User.query.first() # 读取用户记录
     movies = Movie.query.all() # 读取所有电影记录
-    return render_template('index.html', user=user, movies=movies)
-
-# 生成虚拟数据
-# 因为有了数据库，我们可以编写一个命令函数把虚拟数据添加到数据库
+    return render_template('index.html', movies=movies)
 
 
-@app.errorhandler(404) # 穿入要处理的错误代码
-def page_not_found(e): # 接受异常对象作为参数
-    user = User.query.first()
-    return render_template('404.html', user=user), 404 # 返回模版和状态码
+if __name__ == '__main__':
+    app.run()
